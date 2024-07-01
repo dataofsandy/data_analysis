324081292a7b907954c97886da00e77d3ce82e79
codedreamersls <saundaryashrestha@gmail.com>
2024-07-01 21:41:31 +1000

    primary department for each employee

--- advance_select_and_join/2primary department.sql
+++ advance_select_and_join/2primary department.sql
@@ -16,6 +16,33 @@ WITH(
     fieldterminator= ',',
     rowterminator='\n'
 );
+
 delete from employees;
+
 select * from employees;
 
+select 
+    employee_id,
+    department_id
+from employees
+WHERE   
+    primary_flag='y'
+
+union all
+
+SELECT
+    employee_id,
+    department_id
+from employees
+WHERE   
+    primary_flag='n'
+    AND
+    employee_id in (
+        select 
+            employee_id 
+        from 
+            employees 
+        group by 
+            employee_id 
+        having COUNT(employee_id)=1
+    );
